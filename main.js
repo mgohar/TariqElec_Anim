c44ba4bf48317ca87ed1917a886deecbc6b11ffe
Ali Rizvi <alirizvi139@gmail.com>
2023-11-20 19:02:46 +0500

    Fix: animation and responsiveness

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ var geometry = new THREE.TubeGeometry(path, 300, 1, sides, true);
 //===================================================== Basic material
 var material = new THREE.MeshBasicMaterial({
   side: THREE.BackSide,
-  map: new THREE.TextureLoader().load("/tariq_texture.png"),
+  map: new THREE.TextureLoader().load("/assets/images/bg_texture.png"),
 });
 material.map.wrapS = THREE.RepeatWrapping;
 material.map.wrapT = THREE.RepeatWrapping;
@@ -110,42 +110,26 @@ const cityMaterialKioks = new THREE.ShaderMaterial({
 const Kiosk = new THREE.Mesh(cityGeometryKioks, cityMaterialKioks);
 Kiosk.position.z = -100;
 
-const sparkTexture = new THREE.TextureLoader().load("./kenney_particle-pack/transparent/spark_06.png");
+const sparkTexture = new THREE.TextureLoader().load("/static.png");
 
-let sparkGeo1 = new THREE.BoxGeometry(0.01,0.01,1);
-let sparkMat1= new THREE.MeshBasicMaterial({map:sparkTexture,transparent:false});
-let spark1= new THREE.Mesh(sparkGeo1,sparkMat1);
-spark1.position.x=-0.6934780919619608
-spark1.position.y=-0.2
-spark1.position.z=-105
-spark1.rotation.set(-0.00009686608850151691, 0.053745427253361434, 0.0014434275160815147)
-
-let sparkGeo2 = new THREE.BoxGeometry(0.01,0.01,1);
-let sparkMat2= new THREE.MeshBasicMaterial({map:sparkTexture,transparent:false});
-let spark2= new THREE.Mesh(sparkGeo2,sparkMat2);
-spark2.position.x=0.8974731940287006
-spark2.position.y=-0.2
-spark2.position.z=-105
-spark2.rotation.set(-0.00009686608850151691, 0.053745427253361434, 0.0014434275160815147)
-
-let sparkGeo3 = new THREE.BoxGeometry(0.01,0.01,1);
-let sparkMat3= new THREE.MeshBasicMaterial({map:sparkTexture,transparent:false});
-let spark3= new THREE.Mesh(sparkGeo3,sparkMat3);
-spark3.position.x=-0.00164798674274641
-spark3.position.y=0.3994003903464206
-spark3.position.z=-105
-spark3.rotation.set(-0.00009686608850151691, 0.053745427253361434, 0.0014434275160815147)
-
-let sparkGeo4 = new THREE.BoxGeometry(0.01,0.01,1);
-let sparkMat4= new THREE.MeshBasicMaterial({map:sparkTexture,transparent:false});
-let spark4= new THREE.Mesh(sparkGeo4,sparkMat4);
-spark4.position.x=-0.009661595268887713
-spark4.position.y=-0.8429349452070565
-spark4.position.z=-105
-spark4.rotation.set(-0.00009686608850151691, 0.053745427253361434, 0.0014434275160815147)
-
-scene.add(spark1,spark2,spark3,spark4)
-// TControl(spark4)
+// spark4.rotation.set(-0.00009659012301032997, -0.006280995261029449, 0.0014376246587767436)
+let sparkGroup=new THREE.Group();
+for (let i = 0; i < 100; i++) {
+  let sparkGeo1 = new THREE.BoxGeometry(0.005,0.005,1);
+  let sparkMat1= new THREE.MeshBasicMaterial({map:sparkTexture,transparent:false});
+  let spark1= new THREE.Mesh(sparkGeo1,sparkMat1);
+  spark1.position.x= Math.random() * (0.6297152794826579 - (-0.6934780919619608)) +( -0.6934780919619608);
+  spark1.position.y=Math.random() * (0.3772087694566467 - (-0.3990420461252948)) + (-0.3990420461252948);
+  spark1.position.z=Math.random() * ((-109) - (-130)) + (-130);
+  sparkGroup.add(spark1)
+}
+scene.add(sparkGroup);
+// TControl(sparkGroup,"P")
+// scene.add(spark1,spark2,spark3,spark4)
+  // spark5,spark6,spark7,spark8)
+// TControl(spark2,"P")
+// TControl(spark3,"P")
+// TControl(spark4,"P")
 // Kiosk.position.y = -0.2;
 
 const vertexShader0 = `
@@ -168,7 +152,7 @@ const fragmentShader0 = `
   }
 `;
 
-const cityTextureDark = new THREE.TextureLoader().load("/dark_city.png");
+const cityTextureDark = new THREE.TextureLoader().load("/assets/images/model/frame00.png");
 
 const cityGeometry0 = new THREE.BoxGeometry(3, 3, 0.1);
 const cityMaterial0 = new THREE.ShaderMaterial({
@@ -181,8 +165,9 @@ const cityMaterial0 = new THREE.ShaderMaterial({
   transparent: true,
 });
 const city = new THREE.Mesh(cityGeometry0, cityMaterial0);
-city.position.z = 3;
-city.position.y = -0.5;
+city.position.set(0, 0, 8)
+city.scale.set(2.020,1.211)
+
 
 const vertexShader = `
 varying vec2 vUv;
@@ -204,7 +189,7 @@ const fragmentShader = `
   }
 `;
 
-const cityTextureLight = new THREE.TextureLoader().load("/light_city.png");
+const cityTextureLight = new THREE.TextureLoader().load("/assets/images/model/frame0.png");
 
 const cityGeometry1 = new THREE.BoxGeometry(3, 3, 0.1);
 const cityMaterial1 = new THREE.ShaderMaterial({
@@ -218,51 +203,122 @@ const cityMaterial1 = new THREE.ShaderMaterial({
 });
 
 const city1 = new THREE.Mesh(cityGeometry1, cityMaterial1);
-city1.position.z = 3;
-city1.position.y = -0.5;
-scene.add(tube,city, city1, Kiosk);
+city1.position.set(0, 0, 8)
+city1.scale.set(2.020,1.211)
 
-// const model1 = "/objects/hart.gltf";
-// // IMPORT BOTTLE 1 MODEL
-// gltfLoader.load(model1, (gltf) => {
+// CITY GLOW FRAMES
 
-//   tubeObject = gltf.scene;
-//   tubeObject.scale.set(1,1,1)
-//   tubeObject.position.set(0, -0.9994388899135012, -90.23312906124671)
-//   // scene.add(gltf.scene);
-//   const material = new THREE.MeshPhysicalMaterial({
-//     roughness:0.3,
-//     metalness:0,
-//     transmission:1,
-//     ior:2.33
-//    });
+const cityFrameTexture1 = new THREE.TextureLoader().load("/assets/images/model/frame1.png");
+const cityFrameGeo1 = new THREE.BoxGeometry(3, 3, 0.1);
+const cityFrameMaterial1 = new THREE.ShaderMaterial({
+  uniforms: {
+    cityTexture: { value: cityFrameTexture1 },
+    opacity: { value: 0 },
+  },
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+  transparent: true,
+});
+const cityFrame1 = new THREE.Mesh(cityFrameGeo1, cityFrameMaterial1);
+cityFrame1.position.set(0, 0, 8)
+cityFrame1.scale.set(2.020,1.211)
 
-//   tubeObject.traverse((child) => {
-//     if (child.isMesh) {
-//       child.material = material;
-//     }
-//   });
-//   TControl(tubeObject)
+const cityFrameTexture2 = new THREE.TextureLoader().load("/assets/images/model/frame2.png");
+const cityFrameGeo2 = new THREE.BoxGeometry(3, 3, 0.1);
+const cityFrameMaterial2 = new THREE.ShaderMaterial({
+  uniforms: {
+    cityTexture: { value: cityFrameTexture2 },
+    opacity: { value: 0 },
+  },
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+  transparent: true,
+});
+const cityFrame2 = new THREE.Mesh(cityFrameGeo2, cityFrameMaterial2);
+cityFrame2.position.set(0, 0, 8)
+cityFrame2.scale.set(2.020,1.211)
 
-// });
+const cityFrameTexture3 = new THREE.TextureLoader().load("/assets/images/model/frame3.png");
+const cityFrameGeo3 = new THREE.BoxGeometry(3, 3, 0.1);
+const cityFrameMaterial3 = new THREE.ShaderMaterial({
+  uniforms: {
+    cityTexture: { value: cityFrameTexture3 },
+    opacity: { value: 0 },
+  },
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+  transparent: true,
+});
+const cityFrame3 = new THREE.Mesh(cityFrameGeo3, cityFrameMaterial3);
+cityFrame3.position.set(0, 0, 8)
+cityFrame3.scale.set(2.020,1.211)
 
-// const model1_ = "/objects/hart.gltf";
-// // IMPORT BOTTLE 1 MODEL
-// gltfLoader.load(model1_, (gltf) => {
-//   tubeObject_ = gltf.scene;
-//   tubeObject_.position.set(0, -1, -90.23312906124671);
 
-//   scene.add(gltf.scene);
-//   const matcapTexture = new THREE.TextureLoader().load("blue5.png");
-//   const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+const cityFrameTexture4 = new THREE.TextureLoader().load("/assets/images/model/frame4.png");
+const cityFrameGeo4 = new THREE.BoxGeometry(3, 3, 0.1);
+const cityFrameMaterial4 = new THREE.ShaderMaterial({
+  uniforms: {
+    cityTexture: { value: cityFrameTexture4 },
+    opacity: { value: 0 },
+  },
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+  transparent: true,
+});
+const cityFrame4 = new THREE.Mesh(cityFrameGeo4, cityFrameMaterial4);
+cityFrame4.position.set(0, 0, 8)
+cityFrame4.scale.set(2.020,1.211)
 
-//   tubeObject_.traverse((child) => {
-//     if (child.isMesh) {
-//       child.material = material;
-//     }
-//   });
-  
-// });
+
+const cityFrameTexture5 = new THREE.TextureLoader().load("/assets/images/model/frame5.png");
+const cityFrameGeo5 = new THREE.BoxGeometry(3, 3, 0.1);
+const cityFrameMaterial5 = new THREE.ShaderMaterial({
+  uniforms: {
+    cityTexture: { value: cityFrameTexture5 },
+    opacity: { value: 0 },
+  },
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+  transparent: true,
+});
+const cityFrame5 = new THREE.Mesh(cityFrameGeo5, cityFrameMaterial5);
+cityFrame5.position.set(0, 0, 8)
+cityFrame5.scale.set(2.020,1.211)
+
+
+const cityFrameTexture6 = new THREE.TextureLoader().load("/assets/images/model/frame6.png");
+const cityFrameGeo6 = new THREE.BoxGeometry(3, 3, 0.1);
+const cityFrameMaterial6 = new THREE.ShaderMaterial({
+  uniforms: {
+    cityTexture: { value: cityFrameTexture6 },
+    opacity: { value: 0 },
+  },
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+  transparent: true,
+});
+const cityFrame6 = new THREE.Mesh(cityFrameGeo6, cityFrameMaterial6);
+cityFrame6.position.set(0, 0, 8)
+cityFrame6.scale.set(2.020,1.211)
+
+
+const cityFrameTexture7 = new THREE.TextureLoader().load("/assets/images/model/frame7.png");
+const cityFrameGeo7 = new THREE.BoxGeometry(3, 3, 0.1);
+const cityFrameMaterial7 = new THREE.ShaderMaterial({
+  uniforms: {
+    cityTexture: { value: cityFrameTexture7 },
+    opacity: { value: 0 },
+  },
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+  transparent: true,
+});
+const cityFrame7 = new THREE.Mesh(cityFrameGeo7, cityFrameMaterial7);
+cityFrame7.position.set(0, 0, 8)
+cityFrame7.scale.set(2.020,1.211)
+
+
+scene.add(tube,city, city1, Kiosk,cityFrame1,cityFrame2,cityFrame3,cityFrame4,cityFrame5,cityFrame6,cityFrame7);
 
 //===================================================== Create a point light in our scene
 var light = new THREE.PointLight(new THREE.Color("white"), 10, 10);
@@ -271,40 +327,51 @@ var ambientLight = new THREE.AmbientLight("#ffffff",10); // The color of the lig
 scene.add(ambientLight, light);
 
 //===================================================== Animate
+let z1=5;
+let z2=3;
+
+console.log("window.innerWidth",window.innerWidth);
+// if(window.innerWidth>1024){
+//   z1=5;
+//   z2=3;
+// }else if(window.innerWidth>=992){
+//   z1=3;
+//   z2=1;
+// }else if(window.innerWidth>=768){
+//   z1=2;
+//   z2=-2;
+// }else if(window.innerWidth>=576){
+//   z1=2;
+//   z2=-2;
+// }else if(window.innerWidth>=480){
+//   z1=2;
+//   z2=-4;
+// }else if(window.innerWidth>=200){
+  z1=1.8;
+  z2=-6;
+// }
+
+
+
 var exploid = false;
+var tl = gsap.timeline();
+tl.to(camera.position,{z:-105,duration:3,delay:1,ease: "power1.inOut"})
+.to(camera.position,{z:z1,y:-0.6,duration:6,ease: "power3.inOut",onStart:()=>{WhooshAudio()}})
+.to(camera.position,{z:z2,y:0,duration:1,ease: "power3.inOut"})
+gsap.to(sparkGroup.position,{z:180,y:-0.7,duration:6, delay:7,ease: "power2.outIn"})
 
-// gsap.to(camera.position,{z:-100,duration:3,delay:1,ease: "power1.inOut"})
-// gsap.to(camera.position,{z:-1.5,duration:3, delay:3.7,ease: "power2.outIn"})
-// gsap.to(spark1.position,{z:20,duration:5, delay:3.7,ease: "power2.outIn"})
-// gsap.to(spark2.position,{z:20,duration:5, delay:3.7,ease: "power2.outIn"})
-// gsap.to(spark3.position,{z:20,duration:5, delay:3.7,ease: "power2.outIn"})
-// gsap.to(spark4.position,{z:20,duration:5, delay:3.7,ease: "power2.outIn"})
+tl.to(city.material.uniforms.opacity, {value: 0,duration: 0.4,ease: "power1.outIn",},"+=1")
+.to(city1.material.uniforms.opacity, {value: 1,duration: 0.8,ease: "power1.outIn",onComplete:()=>{LightAudio()}},"-=2")
+.to(cityFrame1.material.uniforms.opacity, {value: 1,duration: 0.4 ,ease: "power1.outIn",onStart:()=>{LightAudio()}},"-=0.5")
+.to(cityFrame2.material.uniforms.opacity, {value: 1,duration: 0.4 ,ease: "power1.outIn",onStart:()=>{LightAudio()}})
+.to(cityFrame3.material.uniforms.opacity, {value: 1,duration: 0.4 ,ease: "power1.outIn",onStart:()=>{LightAudio()}})
+.to(cityFrame4.material.uniforms.opacity, {value: 1,duration: 0.4 ,ease: "power1.outIn",onStart:()=>{LightAudio()}})
+.to(cityFrame5.material.uniforms.opacity, {value: 1,duration: 0.4 ,ease: "power1.outIn",onStart:()=>{LightAudio()}})
+.to(cityFrame6.material.uniforms.opacity, {value: 1,duration: 0.4 ,ease: "power1.outIn",onStart:()=>{LightAudio()}})
+.to(cityFrame7.material.uniforms.opacity, {value: 1,duration: 0.4 ,ease: "power1.outIn",onStart:()=>{LightAudio()}});
 
-// gsap.to(city1.scale, {
-//   x: 1.6,
-//   y: 1.6,
-//   delay: 7,
-//   duration: 0.5, // Duration in seconds
-//   ease: "power1.outIn",
-// });
 
-// gsap.to(city.material.uniforms.opacity, {
-//   value: 0,
-//   delay: 7,
-//   duration: 0, // Duration in seconds
-//   ease: "power1.outIn",
-// });
-// gsap.to(city1.material.uniforms.opacity, {
-//   value: 1,
-//   delay: 7,
-//   duration: 0, // Duration in seconds
-//   ease: "power1.outIn",
-//   onUpdate: () => {
-//     exploid = true;
-//     // Render the scene on each update to see the animation
-//     renderer.render(scene, camera);
-//   },
-// });
+
 
 const clock = new THREE.Clock();
 
@@ -370,12 +437,33 @@ function TControl(name,type="P") {
     // The object's position has changed
     const newPosition = name.position;
     const newRotate = name.rotation;
-    type=="R"?
-   ( console.log("New Rotation:", newRotate),tControl.setMode("rotate")):
-    (console.log("New Position:", newPosition),tControl.setMode("translate"));
+    const newScale = name.scale;
+
+    if(type=="R"){
+      tControl.setMode("rotate")
+      console.log("New Rotation:", newRotate)
+    }else if(type=="S"){
+      tControl.setMode("scale")
+      console.log("New Scale:", newScale)
+    }else{
+      tControl.setMode("translate")
+      console.log("New Position:", newPosition)
+    }
+  
   });
 
 
 }
 
 
+function LightAudio() {
+  var audio = document.getElementById("LightAudio");
+  audio.play();
+}
+
+function WhooshAudio() {
+  var audio = document.getElementById("WhooshAudio");
+  setTimeout(() => {
+    audio.play();
+  }, 1500);
+}
